17c2990de88b961c9c83ba8917d4bc27166b4425
kakkarot9712 <vikalpgandha9712@gmail.com>
2022-12-22 19:52:34 +0530

    improve resume render quality and redability

--- src/components/Resume/ResumeComponent.js
+++ src/components/Resume/ResumeComponent.js
@@ -1,22 +1,22 @@
 import * as fromPdfJslib from "pdfjs-dist/webpack";
 import pdfjsWorker from "pdfjs-dist/build/pdf.worker.entry";
-import { useCallback, useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import classes from "./ResumeComponent.module.css";
+import LoadingSpinner from "../UI/LoadingSpinner/LoadingSpinner";
 
 const ResumeComponent = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [err, setErr] = useState(null);
   const url = "../assets/resume.pdf";
   const canvasRef = useRef();
   const renderPDF = useCallback(() => {
-    // The workerSrc property shall be specified.
     fromPdfJslib.GlobalWorkerOptions.workerSrc = pdfjsWorker;
-
-    // Asynchronous download of PDF
     let loadingtask = fromPdfJslib.getDocument(url);
     loadingtask.promise.then(
       (pdf) => {
         let pageNumber = 1;
         pdf.getPage(pageNumber).then((page) => {
-          let scale = 1;
+          let scale = 4;
           let viewport = page.getViewport({ scale: scale });
 
           let canvas = canvasRef.current;
@@ -31,17 +31,30 @@ const ResumeComponent = () => {
 
           let renderTask = page.render(renderContext);
           renderTask.promise.then(() => {});
+          setIsLoading(false);
         });
       },
       (reason) => {
-        console.error(reason);
+        setErr(reason.message);
+        setIsLoading(false);
       }
     );
   }, []);
 
   useEffect(() => {
+    setIsLoading(true);
     renderPDF();
   }, [renderPDF]);
+
+  let content = <LoadingSpinner />;
+
+  if (!isLoading) {
+    content = null;
+  }
+
+  if (!isLoading && err) {
+    content = <h1>{err}</h1>;
+  }
 
   return (
     <section className={classes.section}>
@@ -49,7 +62,8 @@ const ResumeComponent = () => {
         Download Resume
       </a>
       <div className={classes.canvas_container}>
-        <canvas ref={canvasRef}></canvas>
+        {content}
+        <canvas ref={canvasRef} />
       </div>
     </section>
   );
