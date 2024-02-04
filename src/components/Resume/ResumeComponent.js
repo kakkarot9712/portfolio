a7ea66dffb8801f6664d7c6f5f98d392ac3465be
kakkarot9712 <vikalpgandha9712@gmail.com>
2024-02-04 18:00:59 +0530

    update resume and use react-pdf for pdf display

--- src/components/Resume/ResumeComponent.js
+++ src/components/Resume/ResumeComponent.js
@@ -1,69 +1,38 @@
-import * as fromPdfJslib from "pdfjs-dist/webpack";
-import pdfjsWorker from "pdfjs-dist/build/pdf.worker.entry";
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useState } from "react";
 import classes from "./ResumeComponent.module.css";
-import LoadingSpinner from "../UI/LoadingSpinner/LoadingSpinner";
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
+// import LoadingSpinner from "../UI/LoadingSpinner/LoadingSpinner";
+import { pdfjs, Document, Page } from "react-pdf";
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/build/pdf.worker.min.js",
+  import.meta.url
+).toString();
 
 const ResumeComponent = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [err, setErr] = useState(null);
+  const [numPages, setNumPages] = useState();
+  const [pageNumber, setPageNumber] = useState(1);
   const url = "../assets/resume.pdf";
-  const canvasRef = useRef();
-  const renderPDF = useCallback(() => {
-    fromPdfJslib.GlobalWorkerOptions.workerSrc = pdfjsWorker;
-    let loadingtask = fromPdfJslib.getDocument(url);
-    loadingtask.promise.then(
-      (pdf) => {
-        let pageNumber = 1;
-        pdf.getPage(pageNumber).then((page) => {
-          let scale = 4;
-          let viewport = page.getViewport({ scale: scale });
 
-          let canvas = canvasRef.current;
-          let context = canvas.getContext("2d");
-          canvas.height = viewport.height;
-          canvas.width = viewport.width;
-
-          let renderContext = {
-            canvasContext: context,
-            viewport: viewport,
-          };
-
-          let renderTask = page.render(renderContext);
-          renderTask.promise.then(() => {});
-          setIsLoading(false);
-        });
-      },
-      (reason) => {
-        setErr(reason.message);
-        setIsLoading(false);
-      }
-    );
-  }, []);
-
-  useEffect(() => {
-    setIsLoading(true);
-    renderPDF();
-  }, [renderPDF]);
-
-  let content = <LoadingSpinner />;
-
-  if (!isLoading) {
-    content = null;
-  }
-
-  if (!isLoading && err) {
-    content = <h1>{err}</h1>;
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
   }
 
   return (
     <section className={classes.section}>
-      <a href="assets/resume.pdf" target="_blank" rel="norefferer">
+      <a
+        href="assets/resume.pdf"
+        className={classes.downloadBtn}
+        target="_blank"
+        rel="norefferer"
+      >
         Download Resume
       </a>
       <div className={classes.canvas_container}>
-        {content}
-        <canvas ref={canvasRef} />
+        <Document file={{ url }} onLoadSuccess={onDocumentLoadSuccess}>
+          <Page pageNumber={pageNumber} />
+        </Document>
       </div>
     </section>
   );
